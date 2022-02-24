e164c6454a72b82d057e11cb6a9d4a98e70ddf19
liver <liver121888@gmail.com>
2022-02-25 01:01:11 +0800

    Add HW0 test data, finish subtraction.

--- HW0/HW0/HW0_P1.c
+++ HW0/HW0/HW0_P1.c
@@ -1,80 +1,121 @@
-//#include <iostream>
-#include<stdio.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char BigIntsComparator(char a[], char b[], int alength, int blength)
+{
+	char aisbigger = 0;
+	if (alength > blength)
+		aisbigger = 1;
+	else if (alength < blength)
+		goto out;
+	else
+	{
+		for (size_t i = 0; i < alength; i++)
+		{
+			if (a[i] - b[i] > 0)
+			{
+				aisbigger = 1;
+				goto out;
+			}
+			else if (a[i] - b[i] < 0)
+				goto out;
+		}
+	}
+out:
+	return aisbigger;
+}
 
-unsigned char* BigIntsSubtraction(unsigned char a[], unsigned char b[]) {
+unsigned char* BigIntsSubtraction(char a[], char b[])
+{
+	char* bigger, * smaller, * aptr = a, * bptr = b;
+	char c[256];
+	int biggerlength = 0, smallerlength = 0, alength = 0, blength = 0;
+	while (*aptr != 0)
+	{
+		aptr++;
+		alength++;
+	}
+	while (*bptr != 0)
+	{
+		bptr++;
+		blength++;
+	}
+	char isabigger = BigIntsComparator(a, b, alength, blength);
+	if (isabigger == 1)
+	{
+		bigger = a;
+		biggerlength = alength;
+		smaller = b;
+		smallerlength = blength;
+	}
+	else
+	{
+		bigger = b;
+		biggerlength = blength;
+		smaller = a;
+		smallerlength = alength;
+	}
 
-
-    return NULL;
+	int borrower = 0;
+	for (int bb = biggerlength - 1, ss = smallerlength - 1; ss >= 0; bb--, ss--)
+	{
+		int tmp = bigger[bb] - borrower - smaller[ss];
+		if (tmp < 0)
+		{
+			borrower = 1;
+			c[bb] = '0' + (10 + tmp);
+		}
+		else
+		{
+			borrower = 0;
+			c[bb] = '0' + tmp;
+		}
+	}
+	for (size_t i = 0; i < 256; i++)
+	{
+		if (c[i] != '0')
+			break;
+		else
+		{
+			for (size_t j = 0; j < 256; j++)
+			{
+				if (c[j + 1] >= '0')
+					c[j] = c[j + 1];
+				else
+				{
+					c[j] = NULL; //TOOD
+					break;
+				}
+			}
+		}
+	}
+	return c;
 }
 
 unsigned char* BigIntsMultiplication(unsigned char a[], int k) {
 
 
-    return NULL;
+	return NULL;
 }
 
 unsigned char* BigIntsDivision(unsigned char a[], int k) {
 
 
-    return NULL;
+	return NULL;
 }
 
 int main()
 {
-    printf("Hello HWK0\n");
-    int row = 5;
-    int col = 6;
-    int* p = malloc(row * col * sizeof(int));
-    int* q = p;
-    for (int i = 0; i < row * col; i++)
-        *q++ = i;
-    for (int i = 0; i < row * col; i++)
-        printf("%d=%d\n", i, p[i]);
+	FILE* ptr;
+	char c1[256], c2[256];
+	ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW0\\HW0\\hw0_testdata\\gcd\\2.in", "r");
+	fscanf(ptr, "%s %s", c1, c2);
 
-    q = p;
-    int** z = malloc(row * sizeof(int*));
-    for (int r = 0; r < row; r++)
-    {
-        z[r] = q;
-        q += col;
-    }
+	BigIntsSubtraction(c1, c2);
 
-    for (int r = 0; r < row; r++)
-    {
-        for (int c = 0; c < col; c++)
-        {
-            printf("(%d,%d) = %d\n", r, c, z[r][c]);
-        }
-    }
-
-    int ary[3][10];
-    for (int r = 0; r < 3; r++)
-        for (int c = 0; c < 10; c++)
-            ary[r][c] = r * 10 + c;
-
-    void* ptr = malloc(40);
-    int* ip = ptr;
-    for (int i = 0; i < 10; i++)
-        ip[i] = i + 5555555555;
-    for (int i = 0; i < 10; i++)
-        printf("ip[%d] = %d\n", i, ip[i]);
-
-    short* sp = ptr;
-    for (int i = 0; i < 20; i++)
-        printf("sp[%d] = %d\n", i, sp[i]);
-
-    char* cp = ptr;
-    for (int i = 0; i < 40; i++)
-        printf("cp[%d] = %c\n", i, cp[i]);
-
-    float* fp = ptr;
-    for (int i = 0; i < 10; i++)
-        printf("fp[%d] = %f\n", i, fp[i]);
-
-    double* dp = ptr;
-    for (int i = 0; i < 5; i++)
-        printf("dp[%d] = %f\n", i, dp[i]);
-
-
+	fclose(ptr);
+	return 0;
 }
