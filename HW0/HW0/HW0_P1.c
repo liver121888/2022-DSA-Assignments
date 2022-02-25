0984a8a8907d2ae6224a86a348b3c75408c164b5
liver <liver121888@gmail.com>
2022-02-26 00:28:44 +0800

    Add data 50, finish subtraction.

--- HW0/HW0/HW0_P1.c
+++ HW0/HW0/HW0_P1.c
@@ -1,19 +1,23 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #define _CRT_SECURE_NO_WARNINGS
+#define MAX_LENGTH 512
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char BigIntsComparator(char a[], char b[], int alength, int blength)
+int alength, blength, clength;
+
+
+char BigIntsComparator(char a[], char b[], int alen, int blen)
 {
 	char aisbigger = 0;
-	if (alength > blength)
+	if (alen > blen)
 		aisbigger = 1;
-	else if (alength < blength)
+	else if (alen < blen)
 		goto out;
 	else
 	{
-		for (size_t i = 0; i < alength; i++)
+		for (size_t i = 0; i < alen; i++)
 		{
 			if (a[i] - b[i] > 0)
 			{
@@ -28,59 +32,64 @@ out:
 	return aisbigger;
 }
 
-unsigned char* BigIntsSubtraction(char a[], char b[])
+char* BigIntsSubtraction(char a[], char b[], int alen, int blen, int* clen)
 {
-	char* bigger, * smaller, * aptr = a, * bptr = b;
-	char c[256];
-	int biggerlength = 0, smallerlength = 0, alength = 0, blength = 0;
-	while (*aptr != 0)
-	{
-		aptr++;
-		alength++;
-	}
-	while (*bptr != 0)
-	{
-		bptr++;
-		blength++;
-	}
-	char isabigger = BigIntsComparator(a, b, alength, blength);
+	char* bigger, * smaller;
+	char c[MAX_LENGTH];
+	int biggerlength = 0, smallerlength = 0;
+	int _clen = 0;
+
+	char isabigger = BigIntsComparator(a, b, alen, blen);
 	if (isabigger == 1)
 	{
 		bigger = a;
-		biggerlength = alength;
+		biggerlength = alen;
 		smaller = b;
-		smallerlength = blength;
+		smallerlength = blen;
 	}
 	else
 	{
 		bigger = b;
-		biggerlength = blength;
+		biggerlength = blen;
 		smaller = a;
-		smallerlength = alength;
+		smallerlength = alen;
 	}
 
 	int borrower = 0;
-	for (int bb = biggerlength - 1, ss = smallerlength - 1; ss >= 0; bb--, ss--)
+	int tmp = 0;
+	for (int bb = biggerlength - 1, ss = smallerlength - 1; bb >= 0; bb--, ss--)
 	{
-		int tmp = bigger[bb] - borrower - smaller[ss];
-		if (tmp < 0)
+		if (ss < 0)
 		{
-			borrower = 1;
-			c[bb] = '0' + (10 + tmp);
+			c[bb] = bigger[bb] - borrower;
+			borrower = 0;
+			_clen++;
 		}
 		else
 		{
-			borrower = 0;
-			c[bb] = '0' + tmp;
+			tmp = bigger[bb] - borrower - smaller[ss];
+			if (tmp < 0)
+			{
+				borrower = 1;
+				c[bb] = '0' + (10 + tmp);
+			}
+			else
+			{
+				borrower = 0;
+				c[bb] = '0' + tmp;
+			}
+			_clen++;
 		}
 	}
-	for (size_t i = 0; i < 256; i++)
+	c[biggerlength] = 0;
+	//remember to modify the upperbound of i
+	for (size_t i = 0; i < MAX_LENGTH; i++)
 	{
 		if (c[i] != '0')
 			break;
 		else
 		{
-			for (size_t j = 0; j < 256; j++)
+			for (size_t j = 0; j < MAX_LENGTH; j++)
 			{
 				if (c[j + 1] >= '0')
 					c[j] = c[j + 1];
@@ -92,16 +101,53 @@ unsigned char* BigIntsSubtraction(char a[], char b[])
 			}
 		}
 	}
+	*clen = _clen;
 	return c;
 }
 
-unsigned char* BigIntsMultiplication(unsigned char a[], int k) {
-
-
-	return NULL;
+char* BigIntsMultiplication(char* array, int* length, int k)
+{
+	int carrier = 0;
+	int net = 0;
+	//char* c = malloc(sizeof(char) * MAX_LENGTH);
+	char c[260];
+	for (size_t i = 0; i < *length; i++)
+	{
+		c[i] = array[i] - '0';
+	}
+	c[*length] = 0;
+	int clength = 0;
+	for (size_t j = 1; j <= k; j++)
+	{
+		while (c[clength] != 0) clength++;
+		if (c[0] >= 5)
+		{
+			clength = clength + 1;
+		}
+		for (size_t i = clength - 1; i >= 0; i--)
+		{
+			net = (c[i] + carrier) * 2;
+			if (net >= 10)
+			{
+				carrier = 1;
+				c[i] = net - 10;
+			}
+			else
+			{
+				carrier = 0;
+				c[i] = net;
+			}
+		}
+	}
+	for (size_t i = 0; i < clength; i++)
+	{
+		c[i] = c[i] + '0';
+	}
+	return c;
 }
 
-unsigned char* BigIntsDivision(unsigned char a[], int k) {
+unsigned char* BigIntsDivision(unsigned char a[], int k)
+{
 
 
 	return NULL;
@@ -110,11 +156,42 @@ unsigned char* BigIntsDivision(unsigned char a[], int k) {
 int main()
 {
 	FILE* ptr;
-	char c1[256], c2[256];
-	ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW0\\HW0\\hw0_testdata\\gcd\\2.in", "r");
+	char c1[MAX_LENGTH], c2[MAX_LENGTH];
+	//char* c3 = malloc(sizeof(char) * MAX_LENGTH);
+	char* c3;
+	ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW0\\HW0\\hw0_testdata\\gcd\\50.in", "r");
 	fscanf(ptr, "%s %s", c1, c2);
 
-	BigIntsSubtraction(c1, c2);
+	char* aptr = c1, * bptr = c2;
+
+	while (*aptr != 0)
+	{
+		aptr++;
+		alength++;
+	}
+	while (*bptr != 0)
+	{
+		bptr++;
+		blength++;
+	}
+
+	//while (c1[0] != 0 && c2[0] != 0)
+	//{
+	//	if (c1)
+	//	{
+
+	//	}
+	//}
+
+
+
+
+
+	c3 = BigIntsSubtraction(c1, c2, alength, blength, &clength);
+
+	c3 = BigIntsMultiplication(c3, &clength,2);
+
+
 
 	fclose(ptr);
 	return 0;
