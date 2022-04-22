22463b9bacfb526d781c1e5afd476e70c4e56911
iefcyang <iefcyang@gmail.com>
2022-04-22 17:53:32 +0800

    The fast one  added

--- HWK2 P5 Primary and Extra Heaps With DataSets Stored/HWK2 P5 Primary and Extra Heaps With DataSets Stored.c
+++ HWK2 P5 Primary and Extra Heaps With DataSets Stored/HWK2 P5 Primary and Extra Heaps With DataSets Stored.c
@@ -1,6 +1,6 @@
 
 #define _CRT_SECURE_NO_DEPRECATE
-#include "price.h"
+
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -8,7 +8,56 @@
 #include<time.h>
 
 
+unsigned long long price(unsigned long long s, unsigned long long t) {
+    unsigned long long p, x = (s ^ (s >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    p = (s - 1) / 1000000ULL + 1ULL;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+    x = x ^ (x >> 31);
+    unsigned long long la = 0, lb = 0, ra = 0xffffffffffefULL, rb = 0xffffffffffefULL,
+        ma, mb, na, nb, y = x, ta = (t > (p >> 1)) ? (t - (p >> 1)) : 0, tb = t + (p >> 1), tr = t / p;
 
+    for (int i = 28; i >= 1; i -= 3) {
+        ma = la + (((ra - la) * (x >> 48)) >> 16);
+        mb = lb + (((rb - lb) * (y >> 48)) >> 16);
+        if ((1ULL << (i + 2)) & ta) la = ma;
+        else ra = ma;
+        if ((1ULL << (i + 2)) & tb) lb = mb;
+        else rb = mb;
+        ma = la + (((ra - la) * ((x & 0xffff00000000ULL) >> 32)) >> 16);
+        mb = lb + (((rb - lb) * ((y & 0xffff00000000ULL) >> 32)) >> 16);
+        if ((1ULL << (i + 1)) & ta) la = ma;
+        else ra = ma;
+        if ((1ULL << (i + 1)) & tb) lb = mb;
+        else rb = mb;
+        ma = la + (((ra - la) * ((x & 0xffff0000ULL) >> 16)) >> 16);
+        mb = lb + (((rb - lb) * ((y & 0xffff0000ULL) >> 16)) >> 16);
+        x = (x >> 21) ^ (ta & (1ULL << i)) ^ (x * 0xc0ffee123456789ULL);
+        y = (y >> 21) ^ (tb & (1ULL << i)) ^ (y * 0xc0ffee123456789ULL);
+        if ((1ULL << i) & ta) la = ma;
+        else ra = ma;
+        if ((1ULL << i) & tb) lb = mb;
+        else rb = mb;
+    }
+
+    ma = la + (((ra - la) * (x >> 48)) >> 16);
+    mb = lb + (((rb - lb) * (y >> 48)) >> 16);
+    if (1ULL & ta) la = ma;
+    else ra = ma;
+    if (1ULL & tb) lb = mb;
+    else rb = mb;
+    y = (y ^ (y >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+    x = x ^ (y << 13);
+
+    return 	la + tr + (((lb - la) * (x >> 48)) >> 16);
+}
+
+
+typedef struct query
+{
+    unsigned int stockFlag;
+    unsigned int sweetK;
+} Query;
 
 typedef struct poolNode
 {
@@ -21,11 +70,13 @@ typedef struct poolNode
 // Instead of using an object array whose orders are fixed, use pointer array for easier heapify operation
 // During the operation, only swap the pointers is required
 PoolNode** stockHeapArray;
+PoolNode** extraHeapArray;
 
-int numOfStocks, numQuery, increasePeriod, activeNumber, arraySize;
-unsigned long long kSweet, extra;
-unsigned long long* stockIDs, * activeIDs;
-
+int numOfStocks, numQuery, increasePeriod, arraySize;
+Query* queryArray;
+unsigned long long* stockIDs;
+unsigned long long* dataBank;
+unsigned int bankLength;
 
 char answer[80];
 FILE* filePtr;
@@ -34,7 +85,7 @@ FILE* filePtr;
 // Construct the first heap array of PollNode according to their values.
 // It will be min heap, where the root is the smallest and all children are larger than their parents.
 // Once completed the root is the smallest node; yet the tree is not sorted!
-void HeapArrayFirstSort()
+void ConstructHeapArrayAndSetDataBank()
 {
     PoolNode* temp;
     // Start from last parent backward to do min heapify operation
@@ -63,40 +114,11 @@ void HeapArrayFirstSort()
             }
         }
     }
-}
 
-
-// For debug using. Check whether the heap array is a valid min heap
-int CheckHeapArrayValidity(int printFlag)
-{
-    int cid;
-    for (int pid = 0; pid <= arraySize / 2; pid++)
-    {
-        cid = pid * 2 + 1;
-        if (cid < arraySize && stockHeapArray[pid]->value > stockHeapArray[cid]->value)
-        {
-            if (printFlag)  printf("\nERROR! parent H[%d] = %llu  > C[%d] = %llu\n\n", pid, stockHeapArray[pid]->value, cid, stockHeapArray[cid]->value);
-            return 0;
-        }
-        cid++;
-        if (cid < arraySize && stockHeapArray[pid]->value > stockHeapArray[cid]->value)
-        {
-            if (printFlag)  printf("\nERROR! parent H[%d] = %llu  > C[%d] = %llu\n\n", pid, stockHeapArray[pid]->value, cid, stockHeapArray[cid]->value);
-            return 0;
-        }
-    }
-    if (printFlag)   printf("\nHeap array is OK!!\n");
-    return 1;
-}
-
-
-unsigned long long SequentialPoolFiltering(unsigned long long kSweet)
-{
-    PoolNode* temp;
-    unsigned long long k = 1;
-
-    while (k != kSweet)
-    {
+    // Construct the data bank 
+    dataBank[0] = stockHeapArray[0]->value;
+    for( int k = 1; k < bankLength; k++)
+     {
         // The smallest node upgrades to next value
         stockHeapArray[0]->seqID += increasePeriod; // day id jump to next increased day
         unsigned long long v = price(stockHeapArray[0]->stockID, stockHeapArray[0]->seqID); // get upgraded value
@@ -126,42 +148,101 @@ unsigned long long SequentialPoolFiltering(unsigned long long kSweet)
                 childID = parentID * 2 + 1;
             }
         }
-        k++;
-        // Debug
-        //int result = CheckHeapArrayValidity(0);
-        //if(result == 0 )
-        //    printf(" *** Validity Failed at k = %d  \n", k);
-
+        dataBank[k] = stockHeapArray[0]->value;
     }
-
-
-    printf("  My answer => (%llu,%llu) = %llu \n", stockHeapArray[0]->stockID, stockHeapArray[0]->seqID, stockHeapArray[0]->value);
-
-    return stockHeapArray[0]->value;
 }
 
-void PrintHeapArray()
-{
-    for (int i = 0; i < activeNumber * increasePeriod; i++)
-        printf("(%llu,%llu)=%llu ", stockHeapArray[i]->stockID, stockHeapArray[i]->seqID, stockHeapArray[i]->value);
-    printf("\n");
-}
+
+// For debug using. Check whether the heap array is a valid min heap
+//int CheckHeapArrayValidity(int printFlag)
+//{
+//    int cid;
+//    for (int pid = 0; pid <= arraySize / 2; pid++)
+//    {
+//        cid = pid * 2 + 1;
+//        if (cid < arraySize && stockHeapArray[pid]->value > stockHeapArray[cid]->value)
+//        {
+//            if (printFlag)  printf("\nERROR! parent H[%d] = %llu  > C[%d] = %llu\n\n", pid, stockHeapArray[pid]->value, cid, stockHeapArray[cid]->value);
+//            return 0;
+//        }
+//        cid++;
+//        if (cid < arraySize && stockHeapArray[pid]->value > stockHeapArray[cid]->value)
+//        {
+//            if (printFlag)  printf("\nERROR! parent H[%d] = %llu  > C[%d] = %llu\n\n", pid, stockHeapArray[pid]->value, cid, stockHeapArray[cid]->value);
+//            return 0;
+//        }
+//    }
+//    if (printFlag)   printf("\nHeap array is OK!!\n");
+//    return 1;
+//}
+
+
+//unsigned long long SequentialPoolFiltering(unsigned long long kSweet)
+//{
+//    PoolNode* temp;
+//    unsigned long long k = 1;
+//
+//    while (k != kSweet)
+//    {
+//        // The smallest node upgrades to next value
+//        stockHeapArray[0]->seqID += increasePeriod; // day id jump to next increased day
+//        unsigned long long v = price(stockHeapArray[0]->stockID, stockHeapArray[0]->seqID); // get upgraded value
+//        stockHeapArray[0]->value = v; // update value
+//
+//        int parentID = 0; // The root is upgraded
+//        int childID = 1;
+//        // Top-down min heapify??
+//        while (childID < arraySize) // child is traversed one by one
+//        {
+//            // Select the child with the smaller value
+//            if (childID + 1 < arraySize && stockHeapArray[childID + 1]->value < stockHeapArray[childID]->value)
+//                childID++; // second child is smaller than first child
+//            if (stockHeapArray[parentID]->value <= stockHeapArray[childID]->value)
+//            {
+//                break; // Done! Since the parent is smaller or equal to the smaller child
+//            }
+//            else
+//            {
+//                // Let the smaller child turn parent, and downgrade the parent to the child
+//                // Swap child and parent
+//                temp = stockHeapArray[parentID];
+//                stockHeapArray[parentID] = stockHeapArray[childID];
+//                stockHeapArray[childID] = temp;
+//                // Since parent and child are swapped, we need to traverse down further
+//                parentID = childID;
+//                childID = parentID * 2 + 1;
+//            }
+//        }
+//        k++;
+//        // Debug
+//        //int result = CheckHeapArrayValidity(0);
+//        //if(result == 0 )
+//        //    printf(" *** Validity Failed at k = %d  \n", k);
+//
+//    }
+//
+//
+//    printf("  My answer => (%llu,%llu) = %llu \n", stockHeapArray[0]->stockID, stockHeapArray[0]->seqID, stockHeapArray[0]->value);
+//
+//    return stockHeapArray[0]->value;
+//}
+
+//void PrintHeapArray()
+//{
+//    for (int i = 0; i < activeNumber * increasePeriod; i++)
+//        printf("(%llu,%llu)=%llu ", stockHeapArray[i]->stockID, stockHeapArray[i]->seqID, stockHeapArray[i]->value);
+//    printf("\n");
+//}
 
 void main()
 {
-
-    printf("  ******************* SMART HEAP SORTING & Heap INSERT METHOD ******************\n");
+    printf("  ******************* Primary HEAP Get data bank + extra heap  ******************\n");
 
     char fileName1[] = "..\\HWK2 Samples\\p5sample1.txt";
     char fileName2[] = "..\\HWK2 Samples\\p5sample2.txt";
     char fileName3[] = "..\\HWK2 Samples\\p5sample3.txt";
     char fileName4[] = "..\\HWK2 Samples\\p5sample4.txt";
-
     char answer[80];
-
-    // filePtr = fopen(fileName1, "r");
-  //  filePtr = fopen(fileName2, "r");
-  //     filePtr = fopen(fileName3, "r");
 
     for (int f = 1; f <= 4; f++)
     {
@@ -176,7 +257,6 @@ void main()
 
         // Store stock ids in arrays
         stockIDs = malloc((numOfStocks) * sizeof(unsigned long long));
-        activeIDs = malloc((numOfStocks + 1) * sizeof(unsigned long long));
 
         // Allocate heap array that stores the pointers of nodes
         stockHeapArray = malloc((numOfStocks + 1) * increasePeriod * sizeof(PoolNode*));
@@ -186,52 +266,160 @@ void main()
         {
             fscanf(filePtr, "%llu", &id); // or %I64u
             stockIDs[i] = id;
-            activeIDs[i] = stockIDs[i];
         }
 
-        unsigned long long ee = 0, kk = 0;
+        queryArray = malloc(sizeof(Query) * numQuery);
+        bankLength = 0;
 
         for (int j = 0; j < numQuery; j++)
         {
-            fscanf(filePtr, "%llu %llu", &ee, &kk);
-            kSweet = kk;
-            extra = ee;
-            if (extra == 0)
+            fscanf(filePtr, "%llu %llu", &queryArray[j].stockFlag, &queryArray[j].sweetK);
+            if (bankLength < queryArray[j].sweetK) bankLength = queryArray[j].sweetK;
+        }
+
+        // Prepare data bank
+        dataBank = malloc(bankLength * sizeof(unsigned long long));
+
+        arraySize = numOfStocks * increasePeriod;
+
+        // Create Node array
+        int c = 0;
+        for (int s = 0; s < numOfStocks; s++)
+            for (int p = 0; p < increasePeriod; p++)
             {
-                activeNumber = numOfStocks;
+                // Allocate memory for each node and complete the
+                // first non-sorted heap array 
+                stockHeapArray[c] = malloc(sizeof(PoolNode));
+                stockHeapArray[c]->stockID = stockIDs[s];
+                stockHeapArray[c]->seqID = p + 1; // 1 + p * increasePeriod;
+                stockHeapArray[c]->value = price(stockHeapArray[c]->stockID, stockHeapArray[c]->seqID);
+                c++;
+            }
+
+        // Allocate extra memory for the extra stock
+        extraHeapArray = malloc(increasePeriod * sizeof(PoolNode*));
+        for (int i = 0; i < increasePeriod; i++)
+        {
+            extraHeapArray[i] = malloc(sizeof(PoolNode));
+        }
+
+        //Construct the first min-heap array for the standard set of stocks.
+       // Construct data bank of the standard set of stocks
+        ConstructHeapArrayAndSetDataBank();
+
+        // Answer the queries one by one
+
+        for (int i = 0; i < numQuery; i++)
+        {
+            if (queryArray[i].stockFlag == 0)
+            {
+                printf("Query %i  (s=%d k=%d) My answer => %llu \n", i, queryArray[i].stockFlag, queryArray[i].sweetK, dataBank[queryArray[i].sweetK-1]);
             }
             else
             {
-                activeNumber = numOfStocks + 1;
-                activeIDs[numOfStocks] = extra;
-            }
-            arraySize = activeNumber * increasePeriod;
-
-            // Create Node array
-            int c = 0;
-            for (int s = 0; s < activeNumber; s++)
+                // Construct the initial heap array
+                // Set nodes first
                 for (int p = 0; p < increasePeriod; p++)
                 {
-                    // Allocate memory for each node and complete the
                     // first non-sorted heap array 
-                    stockHeapArray[c] = malloc(sizeof(PoolNode));
-                    stockHeapArray[c]->stockID = activeIDs[s];
-                    stockHeapArray[c]->seqID = p + 1; // 1 + p * increasePeriod;
-                    stockHeapArray[c]->value = price(stockHeapArray[c]->stockID, stockHeapArray[c]->seqID);
-                    c++;
+                    extraHeapArray[p]->stockID = queryArray[i].stockFlag;
+                    extraHeapArray[p]->seqID = p + 1; // 1 + p * increasePeriod;
+                    extraHeapArray[p]->value = price(extraHeapArray[p]->stockID, extraHeapArray[p]->seqID);
+                }
+                // Complete the min-heap
+                PoolNode* temp;
+                // Start from last parent backward to do min heapify operation
+                for (int parent = increasePeriod / 2; parent >= 0; parent--)
+                {
+                    int parentID = parent;
+                    int childID = parentID * 2 + 1;
+
+                    while (childID < increasePeriod) // child is traversed one by one
+                    {
+                        // Select the child with the smaller value
+                        if (childID + 1 < increasePeriod && extraHeapArray[childID + 1]->value < extraHeapArray[childID]->value)
+                            childID++; // second child is smaller than first child
+                        if (extraHeapArray[parentID]->value <= extraHeapArray[childID]->value)
+                            break; // Done! Since the parent is smaller or equal to the smaller child
+                        else
+                        {
+                            // Let the smaller child turn parent, and downgrade the parent to the child
+                            // Swap child and parent
+                            temp = extraHeapArray[parentID];
+                            extraHeapArray[parentID] = extraHeapArray[childID];
+                            extraHeapArray[childID] = temp;
+                            // Since parent and child are swapped, we need to traverse down further
+                            parentID = childID;
+                            childID = parentID * 2 + 1;
+                        }
+                    }
                 }
 
+                // 
+                int bankIdx = queryArray[i].sweetK - 1;
+                unsigned long long value = dataBank[bankIdx];
+                unsigned long long nextValue = dataBank[--bankIdx];
+                unsigned long long extraValue = extraHeapArray[0]->value;
+                unsigned long long answer;
 
-            // Sort the firstly constructed heap array to get the first round of values.
-            HeapArrayFirstSort();
-            // Debug            CheckHeapArrayValidity( 1 );
+                while (1)
+                {
+                    if (extraValue > value)
+                    {
+                        answer = value;
+                        break;
+                    }
+                    else if (extraValue > nextValue && extraValue < value)
+                    {
+                        answer = extraValue;
+                        break;
+                    }
+                    else
+                    {
+                        // Lower down data bank one item
+                        value = nextValue;
+                        nextValue = dataBank[--bankIdx];
 
-            printf("Query %d  s=%llu, k=%llu  => ", j, extra, kSweet);
-            unsigned long long answer = SequentialPoolFiltering(kSweet);
+                        // extra heap root upgrade 
+                        // The smallest node upgrades to next value
+                        extraHeapArray[0]->seqID += increasePeriod; // day id jump to next increased day
+                        unsigned long long v = price(extraHeapArray[0]->stockID, extraHeapArray[0]->seqID); // get upgraded value
+                        extraHeapArray[0]->value = v; // update value
 
-            // Debug           printf("Query %d  s=%llu, k=%llu  => answer = %llu \n", j, extra, kSweet, answer);
+                        int parentID = 0; // The root is upgraded
+                        int childID = 1;
+                        // Top-down min heapify??
+                        while (childID < increasePeriod) // child is traversed one by one
+                        {
+                            // Select the child with the smaller value
+                            if (childID + 1 < increasePeriod && extraHeapArray[childID + 1]->value < extraHeapArray[childID]->value)
+                                childID++; // second child is smaller than first child
+                            if (extraHeapArray[parentID]->value <= extraHeapArray[childID]->value)
+                            {
+                                break; // Done! Since the parent is smaller or equal to the smaller child
+                            }
+                            else
+                            {
+                                // Let the smaller child turn parent, and downgrade the parent to the child
+                                // Swap child and parent
+                                temp = extraHeapArray[parentID];
+                                extraHeapArray[parentID] = extraHeapArray[childID];
+                                extraHeapArray[childID] = temp;
+                                // Since parent and child are swapped, we need to traverse down further
+                                parentID = childID;
+                                childID = parentID * 2 + 1;
+                            }
+                        }
+                        extraValue = extraHeapArray[0]->value;
+                    }
+                }
 
+                // 
+                printf("Query %i  (s=%d k=%d) My answer => %llu \n", i, queryArray[i].stockFlag, queryArray[i].sweetK, answer);
+
+            }
         }
+
 
 
         clock_t endTime = clock();
