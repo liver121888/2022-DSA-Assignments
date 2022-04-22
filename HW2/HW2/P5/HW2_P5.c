ec07187aecd9c4984031633f4edd9153cf9ab3d5
liver <liver121888@gmail.com>
2022-04-22 19:55:11 +0800

    Hi.

--- HW2/HW2/P5/HW2_P5.c
+++ HW2/HW2/P5/HW2_P5.c
@@ -7,7 +7,6 @@
 #pragma warning(disable:6385)
 #pragma warning(disable:6386)
 #pragma warning(disable:6011)
-#pragma warning(disable:4101)
 
 typedef struct Qnode
 {
@@ -26,9 +25,7 @@ int FileFlag = 1;
 int DebugFlag = 1;
 int PriceFlag = 1;
 
-int A, Q, N, ES, k, r, StockSN;
-node** StartOfInactivateNode;
-int kcnt = 0;
+int A, Q, N, ES, k, r, StockSN, x;
 int maxk = 0;
 int* StockSerialNumber;
 Qnode* Query;
@@ -36,7 +33,9 @@ Qnode* Query;
 node** PriceHeap;
 node** PriceHeapExtra;
 unsigned long long* SortedPrice;
-unsigned long long* SortedPriceExtra;
+unsigned long long SortedPriceExtra1;
+unsigned long long SortedPriceExtra2;
+//unsigned long long* SortedPriceExtra;
 
 unsigned long long Price(int s, int t)
 {
@@ -46,11 +45,9 @@ unsigned long long Price(int s, int t)
 	}
 	else
 	{
-		return (unsigned long long)(s * t);
+		return (unsigned long long)(s + t);
 	}
 }
-
-
 
 node* tmp;
 void MinHeapify(int current)
@@ -66,8 +63,8 @@ void MinHeapify(int current)
 		// root with child
 		if (2 * current + 2 >= A * N)
 		{
-			// Only left child exist
-			if (tmp->value >= PriceHeap[2 * current + 1]->value)
+			// Only left child exist, >= maybe
+			if (tmp->value > PriceHeap[2 * current + 1]->value)
 			{
 				PriceHeap[current] = PriceHeap[2 * current + 1];
 				PriceHeap[2 * current + 1] = tmp;
@@ -120,6 +117,21 @@ void PrintArray(int select)
 		}
 		printf("\n");
 		break;
+	case(2):
+		// Print PriceHeapExtra
+		printf("PriceHeapExtra (value, stock_id, seq_id): \n");
+		for (int i = 0; i < 2 * N; i++)
+		{
+			printf("(%llu, %d, %d), ", PriceHeapExtra[i]->value, PriceHeapExtra[i]->stock_id, PriceHeapExtra[i]->seq_id);
+		}
+		printf("\n");
+		break;
+	case(3):
+		// Print SortedPriceExtra
+		printf("SortedPriceExtra1, SortedPriceExtra2: \n");
+		printf("%llu, %llu", SortedPriceExtra1, SortedPriceExtra2);
+		printf("\n");
+		break;
 	default:
 		break;
 	}
@@ -137,16 +149,28 @@ void heapSwap(int cur)
 		heapSwap((cur - 1) / 2);
 	}
 }
+void heapSwapExtra(int cur)
+{
+	// if current value is smaller than root, swap them
+	if (PriceHeapExtra[cur]->value < PriceHeapExtra[(cur - 1) / 2]->value)
+	{
+		tmp = PriceHeapExtra[cur];
+		PriceHeapExtra[cur] = PriceHeapExtra[(cur - 1) / 2];
+		PriceHeapExtra[(cur - 1) / 2] = tmp;
+		// recursive swap
+		heapSwapExtra((cur - 1) / 2);
+	}
+}
 
 
 void buildPriceHeap(int cur)
 {
-	tmp = 0;
 	if (cur < A * N)
 	{
 		PriceHeap[cur] = malloc(sizeof(node));
 		PriceHeap[cur]->stock_id = StockSerialNumber[cur / N];
 		PriceHeap[cur]->seq_id = cur % N + 1;
+
 		PriceHeap[cur]->value = Price(PriceHeap[cur]->stock_id, PriceHeap[cur]->seq_id);
 
 		if (cur == 0)
@@ -164,6 +188,109 @@ void buildPriceHeap(int cur)
 	}
 }
 
+int down;
+void assignExtraPriceHeap(int cur)
+{
+	if (cur < 2 * N)
+	{
+		// maybe can be move to outside
+		PriceHeapExtra[cur] = malloc(sizeof(node));
+		PriceHeapExtra[cur]->stock_id = StockSerialNumber[A];
+		if (x-N < 0)
+		{
+			down = 0;
+		}
+		else
+		{
+			down = x - N;
+		}
+		
+		PriceHeapExtra[cur]->seq_id = cur + 1 + down;
+		PriceHeapExtra[cur]->value = Price(PriceHeapExtra[cur]->stock_id, PriceHeapExtra[cur]->seq_id);
+
+		if (cur == 0)
+		{
+			cur++;
+			assignExtraPriceHeap(cur);
+		}
+		else
+		{
+			// compare with root		
+			heapSwapExtra(cur);
+			assignExtraPriceHeap(cur + 1);
+		}
+
+	}
+}
+
+
+
+int bound = 0;
+int heaplen = 0;
+void MinHeapifyExtra(int current)
+{
+	tmp = PriceHeapExtra[current];
+	if (2 * current + 1 >= heaplen)
+	{
+		// single root
+		return;
+	}
+	else
+	{
+		// root with child
+		if (2 * current + 2 >= heaplen)
+		{
+			// Only left child exist, >= maybe
+			if (tmp->value > PriceHeapExtra[2 * current + 1]->value)
+			{
+				PriceHeapExtra[current] = PriceHeapExtra[2 * current + 1];
+				PriceHeapExtra[2 * current + 1] = tmp;
+				MinHeapifyExtra(2 * current + 1);
+			}
+		}
+		else
+		{
+			if (tmp->value >= PriceHeapExtra[2 * current + 1]->value && PriceHeapExtra[2 * current + 1]->value <= PriceHeapExtra[2 * current + 2]->value)
+			{
+				// Both children exist, left is the smallest
+				PriceHeapExtra[current] = PriceHeapExtra[2 * current + 1];
+				PriceHeapExtra[2 * current + 1] = tmp;
+				MinHeapifyExtra(2 * current + 1);
+			}
+			else if (tmp->value >= PriceHeapExtra[2 * current + 2]->value && PriceHeapExtra[2 * current + 2]->value <= PriceHeapExtra[2 * current + 1]->value)
+			{
+				// Both children exist, right is the smallest
+				PriceHeapExtra[current] = PriceHeapExtra[2 * current + 2];
+				PriceHeapExtra[2 * current + 2] = tmp;
+				MinHeapifyExtra(2 * current + 2);
+			}
+			else
+			{
+				// root is the smallest, do nothing
+			}
+		}
+	}
+}
+
+void assign2SortedPriceExtra()
+{
+	if (x - N < 0)
+		bound = x;
+	else
+		bound = N;
+	heaplen = 2 * N;
+
+	for (int i = 0; i < bound; i++)
+	{
+		SortedPriceExtra1 = PriceHeapExtra[0]->value;
+		PriceHeapExtra[0] = PriceHeapExtra[heaplen-1];
+		heaplen--;
+		MinHeapifyExtra(0);
+	}
+	SortedPriceExtra2 = PriceHeapExtra[0]->value;
+}
+
+
 
 void buildSortedPrice()
 {
@@ -176,6 +303,56 @@ void buildSortedPrice()
 	}
 }
 
+unsigned long long binarySeach()
+{
+	int l = 0;
+	int r = k;
+	int m = 0;
+	while (l < r - 1)
+	{
+		m = (l + r) / 2;
+		x = k - m;
+		assignExtraPriceHeap(0);
+		if (DebugFlag)
+		{
+			//PrintArray(2);
+		}
+		assign2SortedPriceExtra();
+		if (DebugFlag)
+		{
+			PrintArray(3);
+			printf("A[m-1]: %llu, A[m]: %llu\n", SortedPrice[m-1], SortedPrice[m]);
+		}
+		// SortedPriceExtra1 = s[k-m], SortedPriceExtra2 = s[k-m+1]
+		if (SortedPriceExtra1 >= SortedPrice[m-1])
+		{
+			if (SortedPriceExtra1 <= SortedPrice[m])
+			{
+				return SortedPriceExtra1;
+			}
+			else
+				l = m;
+		}
+		else if (SortedPrice[m-1] >= SortedPriceExtra1)
+		{
+			if (SortedPrice[m-1] <= SortedPriceExtra2)
+			{
+				return SortedPrice[m-1];
+			}
+			else
+				r = m;
+		}
+	}
+	if (SortedPrice[m] < SortedPriceExtra1)
+	{
+		return SortedPrice[k - 1];
+	}
+	if (SortedPrice[m-1] > SortedPriceExtra1)
+	{
+		return SortedPriceExtra1;
+	}
+}
+
 
 // We need to find the smallest or particularly extent of small across all stocks
 int main()
@@ -184,7 +361,7 @@ int main()
 	FILE* ptr = 0;
 	if (FileFlag)
 	{
-		ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW2\\HW2\\hw2_testdata\\P5\\2.in", "r");
+		ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW2\\HW2\\hw2_testdata\\P5\\3.in", "r");
 		r = fscanf(ptr, "%d %d %d", &A, &Q, &N);
 	}
 	else
@@ -192,13 +369,16 @@ int main()
 		r = scanf("%d %d %d", &A, &Q, &N);
 	}
 
-
 	// There may be extra stock
-	StockSerialNumber = malloc(sizeof(int) * (A + 1));
+	StockSerialNumber = malloc(sizeof(int) * (A + 1) * N);
 	PriceHeap = malloc(sizeof(node*) * A * N);
-	PriceHeapExtra = malloc(sizeof(unsigned long long) * (A + 1) * N);
+	// k-m-N+2 < k-m+1 < k-m+N
+	// k-m-N+1 < k-m < k-m+N-1
+	// the thing we want would be the Nth smallest number in extra heap
+	// it might be k-m+1 or k-m
+	// so the size should be 2N+1
+	PriceHeapExtra = malloc(sizeof(node*) * (2 * N));
 	Query = malloc(sizeof(Qnode) * Q);
-
 
 	for (int i = 0; i < A; i++)
 	{
@@ -212,9 +392,6 @@ int main()
 		}
 		StockSerialNumber[i] = StockSN;
 	}
-
-	// Extra stock assign -1 to distingusih extra or not
-	StockSerialNumber[A] = -1;
 	
 	// read all queries
 	for (int i = 0; i < Q; i++)
@@ -237,7 +414,7 @@ int main()
 
 	if (DebugFlag)
 	{
-		PrintArray(0);
+		//PrintArray(0);
 	}
 
 	SortedPrice = malloc(sizeof(unsigned long long) * maxk);
@@ -251,7 +428,6 @@ int main()
 	// deal with queries
 	for (int i = 0; i < Q; i++)
 	{
-
 		if (Query[i].ES == 0)
 		{
 			// minus 1 to get the index
@@ -259,15 +435,10 @@ int main()
 		}
 		else
 		{
-
 			// Build extra stock
-			StockSerialNumber[A] = ES;
-			// At the end of extra query, clear extra stock
-			if (ES != 0)
-			{
-				// clear extra stock
-				StockSerialNumber[A] = -1;
-			}
+			StockSerialNumber[A] = Query[i].ES;
+			printf("%llu\n", binarySeach());
+			// Clear extra stock, SortedPriceExtra?
 		}
 	}
 
--- HW2/HW2/P5/price.h
+++ HW2/HW2/P5/price.h
@@ -3,8 +3,7 @@ If you are testing, price(s, t + n) > price(s, t) for all n >= (s - 1) / 1000000
 In other words, the "N" in the problem description is equal to (s - 1) / 1000000 + 1 
 
 Notice that, this function is slightly slower than promised. */
-#pragma warning(push)
-#pragma warning(disable:4101)
+
 unsigned long long price(unsigned long long s, unsigned long long t){
 	unsigned long long p, x = (s ^ (s >> 30)) * 0xbf58476d1ce4e5b9ULL;
 	p = (s - 1) / 1000000ULL + 1ULL;
@@ -48,4 +47,3 @@ unsigned long long price(unsigned long long s, unsigned long long t){
 
 	return 	la + tr + (((lb - la) * (x >> 48)) >> 16);
 }
-#pragma   warning(pop)  
