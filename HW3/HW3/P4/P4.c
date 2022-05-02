9bc8b7ced303d3e09a528ce899e722f601315a2c
liver <liver121888@gmail.com>
2022-05-02 21:49:59 +0800

    Finish P4.

--- HW3/HW3/P4/P4.c
+++ HW3/HW3/P4/P4.c
@@ -3,39 +3,91 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #pragma warning(push)
 #pragma warning(disable:6385)
 #pragma warning(disable:6386)
 #pragma warning(disable:6011)
 
 // References:
-// Ref: https://www.programiz.com/dsa/graph-adjacency-list
-// For the building of adjacency list to store the info of city connection
-// Ref: https://www.geeksforgeeks.org/lca-n-ary-tree-constant-query-o1/
+// Ref: https://www.geeksforgeeks.org/minimum-characters-added-front-make-string-palindrome/?ref=lbp
+// Ref: https://www.geeksforgeeks.org/minimum-number-appends-needed-make-string-palindrome/?ref=lbp
 // For finding the LCA of two nodes
 
 
-typedef struct node {
-	struct node* next;
-	int id;
-}node;
-
-typedef struct graph {
-	int nodenumber;
-	int* visited;
-	struct node** adjlists;
-}graph;
-
 int FileFlag = 1;
-int DebugFlag = 1;
+int DebugFlag = 0;
 int re;
+char s[10 * 1000 * 1000 + 1];
+char* concat;
+int* lps;
+int concatlen;
+int len;
 
-int CityNumber, QueryNumber, CapitalNumber, ResortNumber;
-int CityA, CityB, CityC;
-int* MinDepthCity;
-int EulerStamp = 0;
+int computeLPSArray(char* c)
+{
+	int index = 0;
+	lps[0] = 0;
 
-graph* citygraph;
+	// the loop calculates lps[i] for i = 1 to M-1
+	int i = 1;
+	while (i < concatlen)
+	{
+		if (c[i] == c[index])
+		{
+			index++;
+			lps[i] = index;
+			i++;
+		}
+		else // (str[i] != str[len])
+		{
+			// This is tricky. Consider the example.
+			// AAACAAAA and i = 7. The idea is similar
+			// to search step.
+			if (index != 0)
+			{
+				index = lps[index - 1];
+
+				// Also, note that we do not increment
+				// i here
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+	return len - lps[concatlen - 1];
+}
+
+int Compute_Prefix_Function(char* P)
+{
+	int k = 0;
+	lps[0] = 0;
+	for (int q = 1; q < concatlen; q++)
+	{
+		while (k > 0 && P[k] != P[q])
+		{
+			k = lps[k - 1];
+		}
+		if (P[k] == P[q])
+			k = k + 1;
+		lps[q] = k;
+	}
+	if (DebugFlag)
+	{
+		printf("lps: \n");
+		for (int i = 0; i < concatlen; i++)
+			printf("%d, ", lps[i]);
+		printf("\n");
+	}
+	return len - lps[concatlen - 1];
+}
+
+
+
+
 
 
 int main()
@@ -43,68 +95,99 @@ int main()
 	FILE* ptr = 0;
 	if (FileFlag)
 	{
-		ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW3\\HW3\\hw3_testdata\\P4\\0.in", "r");
-		re = fscanf(ptr, "%d %d %d %d", &CityNumber, &QueryNumber, &CapitalNumber, &ResortNumber);
+		ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW3\\HW3\\hw3_testdata\\P4\\1.in", "r");
+		re = fscanf(ptr, "%s", &s);
 	}
 	else
-		re = scanf("%d %d %d %d", &CityNumber, &QueryNumber, &CapitalNumber, &ResortNumber);
-
-	citygraph = malloc(sizeof(graph));
-	citygraph->nodenumber = CityNumber;
-	citygraph->adjlists = malloc(CityNumber * sizeof(node*));
-	citygraph->visited = malloc(CityNumber * sizeof(int));
-	MinDepthCity = malloc(sizeof(int) * CityNumber);
-
-	for (int i = 0; i < CityNumber; i++)
-	{
-		citygraph->adjlists[i] = NULL;
-		citygraph->visited[i] = 0;
-		MinDepthCity[i] = -1;
-	}
-
-	int start, end;
-	for (int k = 0; k < CityNumber - 1; k++)
-	{
-		if (FileFlag)
-			re = fscanf(ptr, "%d %d", &start, &end);
-		else
-			re = scanf("%d %d", &start, &end);
-		// minus 1 to cope with the index and the numbering difference
-		start--;
-		end--;
-		connect(start, end);
-	}
-	// at the start of every list, add itself
-	for (int k = 0; k < CityNumber; k++)
-	{
-		node* nself = malloc(sizeof(node));
-		nself->id = k;
-		nself->next = citygraph->adjlists[k];
-		citygraph->adjlists[k] = nself;
-	}
+		re = scanf("%s", &s);
+	len = strlen(s);
+	concatlen = 2 * len + 1;
+	concat = malloc(sizeof(char) * (concatlen));
+	// reversed is insert at front
+	for (int i = 0; i < len; i++)
+		concat[i] = s[len - 1 - i];
+	concat[len] = 7;
+	for (int i = len + 1; i < concatlen; i++)
+		concat[i] = s[i - (len + 1)];
+	
 
 	if (DebugFlag)
 	{
-		printCity();
+		for (int i = 0; i < concatlen; i++)
+			printf("%c, ", concat[i]);
+		printf("\n");
 	}
 
-	// minus 1 to cope with the index and the numbering difference
-	int Re = ResortNumber - 1;
-	int Ca = CapitalNumber - 1;
-
-	int q;
-	for (int k = 0; k < QueryNumber; k++)
+	lps = malloc(sizeof(int) * (concatlen));
+	int b = Compute_Prefix_Function(concat);
+	if (DebugFlag)
 	{
-		if (FileFlag)
-			re = fscanf(ptr, "%d", &q);
-		else
-			re = scanf("%d", &q);
-		// minus 1 to cope with the index and the numbering difference
-		q--;
-		// plus 1 to cope with the index and the numbering difference
-		printf("%d\n", MinDepthCity[q] + 1);
+		printf("b: %d\n", b);
 	}
 
+	// reversed is insert at back
+	for (int i = 0; i < len; i++)
+	{
+		concat[i] = s[i];
+	}
+	concat[len] = 7;
+	for (int i = len + 1; i < concatlen; i++)
+	{
+		concat[i] = s[concatlen - i - 1];
+	}
+	if (DebugFlag)
+	{
+		for (int i = 0; i < concatlen; i++)
+			printf("%c, ", concat[i]);
+		printf("\n");
+	}
+
+	int f = Compute_Prefix_Function(concat);
+	if (DebugFlag)
+	{
+		printf("f: %d\n", f);
+	}
+
+
+
+	if (f == b)
+	{
+		printf("%d\n", f);
+		for (int i = 0; i < f; i++)
+		{
+			printf("%c", s[len - 1 - i]);
+		}
+		printf("%s\n", s);
+
+		if (f != 0)
+		{
+			printf("%s", s);
+			for (int i = b - 1; i >= 0; i--)
+			{
+				printf("%c", s[i]);
+			}
+			printf("\n");
+		}
+	}
+	else if (f < b)
+	{
+		printf("%d\n", f);
+		for (int i = 0; i < f; i++)
+		{
+			printf("%c", s[len - 1 - i]);
+		}
+		printf("%s\n", s);
+	}
+	else
+	{
+		printf("%d\n", b);
+		printf("%s", s);
+		for (int i = b - 1; i >= 0; i--)
+		{
+			printf("%c", s[i]);
+		}
+		printf("\n");
+	}
 
 	if (FileFlag && ptr != NULL)
 	{
