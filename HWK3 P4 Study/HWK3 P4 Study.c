443f928b51ad89569633bec243a621ed97ce9e91
iefcyang <iefcyang@gmail.com>
2022-05-02 11:41:35 +0800

    Done with P4

--- HWK3 P4 Study/HWK3 P4 Study.c
+++ HWK3 P4 Study/HWK3 P4 Study.c
@@ -4,12 +4,29 @@
 #include<math.h>
 #include<time.h>
 
+
+//  aabc
+//  Answer :
+//  2
+//  cbaabc
+//  
+//  baaaac
+//  Answer :
+//  5
+//  caaaabaaaac
+//  baaaacaaaab
+//  
+//  ababa
+//  Answer :
+//  0
+//  ababa
+
+
+char s[10000000];
 int length;
 unsigned char low = 0x21;
 unsigned char up = 0x7E;
 int range = 0x7E - 0x21 + 1;
-
-char s[10000000];
 
 void main()
 {
@@ -21,20 +38,27 @@ void main()
         s[length++] = c - low;
     }
 
-
-    long long Target1, Target2;
-    long long Part1, Part2;
-
+    // Target1+Part1 for suffix palindrome; Target1 start from s[0] and shift to right step by step.
+    // Part1 reverses from the last characters.
+    long long Target1, Part1 ;
     int t1Start = 0;
     int p1Start = length - 1;
-    int p2Start = 0;
-    int t2Start = length - 1;
 
+    // Target2+Part2 for prefix palindrome; Target2 reversely starts from the last characters and shift to left step by step.
+    // Part2 starts from s[0];
+    long long Target2, Part2;
+    int t2Start = length - 1;
+    int p2Start = 0;
+
+    // Module value set
     long long module = pow(2, 8 * sizeof(long long)) / range;
 
-    int active = length;
+    // The current active length covering target length, part length, and possible a middle character
+    int active = length; 
+    // Size is the current character length of targets and parts subject to comparisons.
     int size = active / 2;
 
+    // Prepare the beginning modulus values of Target1, Part1, Target2, and Part2.
     Target1 = 0;
     int final = t1Start + size;
     for (int i = t1Start; i < final; i++)
@@ -50,115 +74,124 @@ void main()
         Part1 = (Part1 * range + s[i]) % module;
     }
     Target2 = Part1;
+
+    // newSize is updated when current matching fails and active length reduced by one.
+    // Note that updated newSize is alternatively remaining the same as Size or reduced by one
     int newSize;
-    for (int d = 0; size >= 0; d++)
+
+    // Flag for signaling that either pre- or suf- palindrome is found; no further comparison is necessary
+    int minimalAnswerFound = 0;
+
+    // Try all steps until Size is reduced to zero
+    for (int shift = 0; size >= 0; shift++)
     {
-        if (size == 0)
+        if (size == 0) // No any match is found
         {
             //prefix
-            printf("%d\n", d);
-            for (int i = 0, j = length - 1; i < d; i++, j--) printf("%c", s[j] + low);
+            printf("%d\n", shift);
+            for (int i = 0, j = length - 1; i < shift; i++, j--) printf("%c", s[j] + low);
             for (int i = 0; i < length; i++) printf("%c", s[i] + low);
             printf("\n");
 
             // suffix
             for (int i = 0; i < length; i++) printf("%c", s[i] + low);
-            for (int i = d - 1; i >= 0; i--) printf("%c", s[i] + low);
+            for (int i =0, j = shift - 1; i < shift; i++, j--) printf("%c", s[j] + low);
             printf("\n");
 
-            break;
+            break; // No further search.
         }
 
-        int hit = 1;
+
+        int hit;
         if (Target2 == Part2)
         {
+            hit = 1;
             // Check matching
             for (int t = t2Start, p = p2Start, z = 0; z < size; z++, t--, p++)
             {
                 if (s[t] != s[p])
                 {
-                    hit = 0;
+                    hit = 0; // not match
                     break;
                 }
             }
             if (hit)
             {
-                // Prefix s[length-1-d] reversely
-                // print out results
-                printf("%d\n", d);
-                for (int i = 0, j = length - 1; i < d; i++, j--) printf("%c", s[j] + low);
+                // Prefix s[length-1-d] reversely; print out results
+                printf("%d\n", shift);
+                for (int i = 0, j = length - 1; i < shift; i++, j--) printf("%c", s[j] + low);
                 for (int i = 0; i < length; i++) printf("%c", s[i] + low);
                 printf("\n");
-                break;
+                if (shift == 0) break;  // The original is palindrome; just report it and exit
             }
+            minimalAnswerFound = 1; // Prefix solution found!
         }
-
 
         if (Target1 == Part1)
         {
-            // Check matching
+         hit = 1;
+           // Check matching
             for (int t = t1Start, p = p1Start, z = 0; z < size; z++, t++, p--)
             {
                 if (s[t] != s[p])
                 {
-                    hit = 0;
+                    hit = 0; // not match
                     break;
                 }
             }
             if (hit)
             {
-                //  Suffix s[0-d] reversely
-                // print out results
-                printf("%d\n", d);
+                //  Suffix s[0-d] reversely ; print out results
+                printf("%d\n", shift);
                 for (int i = 0; i < length; i++) printf("%c", s[i] + low);
-                for (int i = d - 1; i >= 0; i--) printf("%c", s[i] + low);
+                for (int i = 0, j= shift - 1; i < shift; i++, j--) printf("%c", s[j] + low);
                 printf("\n");
-                if (d == 0) break;
+                minimalAnswerFound = 1; // Suffix solution found!
             }
-
         }
 
+        // Either prefix or suffix solution found, exit; no larger number palindromes are searched. 
+        if(minimalAnswerFound) break;  
 
+
+        // This value comparison checking (pattern matching) fails; Shift targets, reduce active length, update size to
+        // calculate new values of targets and parts.
         active--;
-        newSize = active / 2;
+        newSize = active / 2; // newSize updated, alternatively reduced by one or remaining the same.
 
-        // prefix part
-        Part2 = (Part2 - s[p2Start] * (int)(pow(range, size - 1))) % module;
-        Part2 = Part2 % module;
+        // prefix part; target shifts to left one character
+        Target2 = (Target2 - ( s[t2Start] * (int)(pow(range, size - 1))) % module )  % module;
         if (newSize == size)
         {
-            // Add new last term
-            Part2 += s[p2Start + size];
+            // add new last term, since size is not reduced
+            Target2 += s[t2Start - size];
         }
         else
         {
-            // size reduced; delete the last term
-            Target2 = (Target2 - s[t2Start - size + 1]) / range;
+            // Value of part should be reduced, since newSize is reduced
+            Part2 = (Part2 - s[p2Start + size - 1]) / range;
         }
-
+        // Formally shifting to left
         t2Start--;
 
-
-        // suffix part
+        // suffix part; target shifts to right one character
         //Target1 backward by 1
-        Target1 = (Target1 - s[t1Start] * (int)(pow(range, size - 1))) % module;
-        Target1 = Target1 % module;
+        Target1 = (Target1 - ( s[t1Start] * (int)(pow(range, size - 1))) % module ) % module;
         if (newSize == size)
         {
-            // Add new last term
+            // Add new last term, since size is not reduced
             Target1 += s[t1Start + size];
         }
         else
         {
-            // size reduced; delete the last term
+            // Value of part should be reduced, since newSize is reduced
             Part1 = (Part1 - s[p1Start - size + 1]) / range;
         }
-
+        // Formally shifting to right
         t1Start++;
 
+        // Update size for next step value comparision
         size = newSize;
-
-
     }
 
 
