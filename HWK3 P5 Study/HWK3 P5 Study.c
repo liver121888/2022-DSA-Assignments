acaaf098de8dbe7ad9f37e984f03a12998aae671
烽正 楊 <iefcyang@gmail.com>
2022-05-09 23:45:48 +0800

    start P5

---
+++ HWK3 P5 Study/HWK3 P5 Study.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+
+//  aabc
+//  Answer :
+//  2
+//  cbaabc
+//  
+//  baaaac
+//  Answer :
+//  5
+//  caaaabaaaac
+//  baaaacaaaab
+//  
+//  ababa
+//  Answer :
+//  0
+//  ababa
+
+
+char s[10000010];
+int length;
+unsigned char low = 0x21;
+unsigned char up = 0x7E;
+int range = 0x7E - 0x21 + 1;
+
+void main()
+{
+}
