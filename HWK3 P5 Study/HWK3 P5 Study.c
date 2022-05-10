238b94711791e2c477c312972ab32610558efe79
iefcyang <iefcyang@gmail.com>
2022-05-10 11:08:48 +0800

    2022 05 10

--- HWK3 P5 Study/HWK3 P5 Study.c
+++ HWK3 P5 Study/HWK3 P5 Study.c
@@ -27,7 +27,20 @@ int length;
 unsigned char low = 0x21;
 unsigned char up = 0x7E;
 int range = 0x7E - 0x21 + 1;
+int number, size, flag;
+char* all, ** subString;
 
 void main()
 {
+	scanf("%d %d %d", &number, &size, &flag);
+	// allocate a chuck of contijous memory.
+	all = malloc(sizeof(char) * number * size);
+	subString = malloc(sizeof(char*) * number);
+	for (int i = 0; i < number; i++)
+	{
+		subString[i] = all + i * size;
+		scanf("%s", subString[i]);
+	}
+
+
 }
