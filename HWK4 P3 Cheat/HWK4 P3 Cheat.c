c97c61a2fa6431d945560a1c78570a6366569377
iefcyang <iefcyang@gmail.com>
2022-05-27 13:26:53 +0800

    HWK4 P3 are solved in new projects: HWK4 P3 ArraySolution and HWK4 P3 Cheat.

---
+++ HWK4 P3 Cheat/HWK4 P3 Cheat.c
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+
+int N, M;
+char cmd[20];
+int* groupCounts;
+
+void main()
+{
+	int boomDay;
+	int first, second;
+
+	scanf("%d %d", &N, &M);
+
+	groupCounts = malloc(sizeof(int) * (M + 1));
+	groupCounts[0] = N;
+
+	int currentGroupCount = N;
+	for (int day = 1; day <= M; day++)
+	{
+		scanf("%s", cmd);
+		switch (cmd[0])
+		{
+		case 'm': // merge
+			scanf("%d %d", &first, &second);
+			currentGroupCount--;
+			break;
+		case 'q': // query
+			printf("%d\n", currentGroupCount);
+			break;
+		case 'b': // boom
+			scanf("%d", &boomDay);
+			currentGroupCount = groupCounts[boomDay];
+			break;
+		}
+		// Store groupCount of this day
+		groupCounts[day] = currentGroupCount;
+	}
+
+}
