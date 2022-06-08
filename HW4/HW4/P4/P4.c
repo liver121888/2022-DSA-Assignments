5150dbe22a3b1a0a6ca157b1633d707b5a699be1
liver <liver121888@gmail.com>
2022-06-08 16:05:16 +0800

    Add Nick version.

--- HW4/HW4/P4/P4.c
+++ HW4/HW4/P4/P4.c
@@ -8,20 +8,22 @@
 #include <string.h>
 
 #pragma warning(push)
-#pragma warning(disable:6385)
-#pragma warning(disable:6386)
-#pragma warning(disable:6011)
+//#pragma warning(disable:6385)
+//#pragma warning(disable:6386)
+//#pragma warning(disable:6011)
 
 typedef struct node {
-    int priority, size;
+    long long priority, size;
     bool rev;
     long long bootTime, timeSum, lazy;
+    long long maxBT, secondBT;
+    long long maxCnt;
     struct node* left, *right;
 } Node;
 
 int fileFlag = 1;
-int debugFlag = 1;
-int numMachineN, numRecordQ;
+int debugFlag = 0;
+long long numMachineN, numRecordQ;
 Node* server;
 
 // ref: https://www.geeksforgeeks.org/treap-set-2-implementation-of-search-insert-and-delete/
@@ -40,20 +42,21 @@ void printTreap(Node* root, bool isFirst)
     if (root)
     {
         printTreap(root->left, false);
-        printf("bT: %lld | priority: %d | size: %d | lazy: %lld | timeSum: %lld ", 
-            root->bootTime, root->priority, root->size, root->lazy, root->timeSum);
+        printf("bT: %lld | pri: %lld | sz: %lld | lazy: %lld | tS: %lld | mBT: %lld | sBT: %lld | mC: %lld ", 
+            root->bootTime, root->priority, root->size, root->lazy, root->timeSum,
+            root->maxBT, root->secondBT, root->maxCnt);
         if (root->left)
-            printf("| left: %lld ", root->left->bootTime);
+            printf("| left: %lld, l_pri: %lld ", root->left->bootTime, root->left->priority);
         if (root->right)
-            printf("| right: %lld ", root->right->bootTime);
+            printf("| right: %lld, r_pri: %lld ", root->right->bootTime, root->right->priority);
         printf("\n");
         printTreap(root->right, false);
     }
 }
 
-int getSize(Node* t)
+long long getSize(Node* t)
 {
-    int returnValue = t == NULL ? 0 : t->size;
+    long long returnValue = t == NULL ? 0 : t->size;
     return returnValue;
 }
 
@@ -63,16 +66,87 @@ long long getTimeSum(Node* t)
     return returnValue;
 }
 
-void pull(Node* t)
+long long getMaxBT(Node* t)
+{
+    long long returnValue = t == NULL ? 0 : t->maxBT;
+    return returnValue;
+}
+
+long long getMaxCnt(Node* t)
+{
+    long long returnValue = t == NULL ? 0 : t->maxCnt;
+    return returnValue;
+}
+
+long long getSecondBT(Node* t)
+{
+    long long returnValue = t == NULL ? 0 : t->secondBT;
+    return returnValue;
+}
+
+
+void jiry(Node* t, long long k)
+{
+    if (t->bootTime > k)
+    {
+        t->bootTime = k;
+
+    }
+    else
+    {
+        t->bootTime = t->bootTime;
+    }
+    t->timeSum -= t->maxCnt * (t->maxBT - k);
+    t->maxBT = k;
+
+    if (t->left != NULL)
+    {
+        if (t->left->lazy == 0)
+            t->left->lazy = k;
+        else
+            //t->left->lazy = t->lazy;
+            t->left->lazy = t->left->lazy < k ? t->left->lazy : k;
+    }
+    if (t->right != NULL)
+    {
+        if (t->right->lazy == 0)
+            t->right->lazy = k;
+        else
+            //t->right->lazy = t->lazy
+            t->right->lazy = t->right->lazy < k ? t->right->lazy : k;
+    }
+    t->lazy = 0;
+}
+
+
+
+// At first I try to use only one push function incorporating lazy and rev operations, 
+// but that is chaotic and hard to debug, not to mention sometimes we need to check 
+// whether we should do lazy operations, so I discuss with Nick and change it to two functions 
+// for better reusability.
+Node* tmp;
+long long vtmp;
+void pushLazy(Node* t)
 {
     if (t == NULL)
         return;
-    t->size = 1 + getSize(t->left) + getSize(t->right);
-    t->timeSum = t->bootTime + getTimeSum(t->left) + getTimeSum(t->right);
+    if (t->lazy == 0)
+        return;
+
+    if (t->maxBT <= t->lazy)
+    {
+        t->lazy = 0;
+        return;
+    }
+    else
+    {
+        jiry(t, t->lazy);
+        return;
+    }
 }
 
-Node* tmp;
-void push(Node* t)
+
+void pushRev(Node* t)
 {
     if (t == NULL)
         return;
@@ -87,14 +161,243 @@ void push(Node* t)
             t->right->rev ^= 1;
         t->rev = 0;
     }
-    t->bootTime += t->lazy;
-    t->timeSum += t->lazy;
-    if (t->left != NULL)
-        t->left->lazy += t->lazy;
-    if (t->right != NULL)
-        t->right->lazy += t->lazy;
-    t->lazy = 0;
 }
+
+
+
+// ref: NICK, mine fighting pull is too hard to complete, and hard to debug
+// I discuss with my classmate Nick and I appreciate the divide and conquer method Nick used.
+void pull(Node* t)
+{
+    if (t == NULL)
+        return;
+    pushRev(t);
+    pushLazy(t);
+    // reset to calculate
+    //t->size = 1;
+    //t->timeSum = t->bootTime;
+    // TODO: reset below or not? no.
+    t->maxBT = t->bootTime;
+    t->secondBT = 0;
+    t->maxCnt = 1;
+    if (t->left)
+    {
+        pushLazy(t->left);
+    }
+    if (t->right)
+    {
+        pushLazy(t->right);
+    }
+
+    t->size = 1 + getSize(t->left) + getSize(t->right);
+    t->timeSum = t->bootTime + getTimeSum(t->left) + getTimeSum(t->right);
+    // separate to two groups to judge
+    if (t->left)
+    {
+        if (t->left->maxBT > t->maxBT)
+        {
+            t->secondBT = t->left->secondBT > t->maxBT ? t->left->secondBT : t->maxBT;
+            t->maxBT = t->left->maxBT;
+            t->maxCnt = t->left->maxCnt;
+        }
+        else if (t->left->maxBT == t->maxBT)
+        {
+            t->maxCnt += t->left->maxCnt;
+            t->secondBT = t->left->secondBT > t->secondBT ? t->left->secondBT : t->secondBT;
+        }
+        else
+        {
+            t->secondBT = t->left->maxBT > t->secondBT ? t->left->maxBT : t->secondBT;
+        }
+    }
+
+    if (t->right)
+    {
+        if (t->right->maxBT > t->maxBT)
+        {
+            t->secondBT = t->right->secondBT > t->maxBT ? t->right->secondBT : t->maxBT;
+            t->maxBT = t->right->maxBT;
+            t->maxCnt = t->right->maxCnt;
+        }
+        else if (t->right->maxBT == t->maxBT)
+        {
+            t->maxCnt += t->right->maxCnt;
+            t->secondBT = t->right->secondBT > t->secondBT ? t->right->secondBT : t->secondBT;
+        }
+        else
+        {
+            t->secondBT = t->right->maxBT > t->secondBT ? t->right->maxBT : t->secondBT;
+        }
+    }
+}
+
+//long long m, noZero;
+//long long mc;
+//long long maxtmp;
+//long long maxcnttmp;
+//long long secondtmp;
+//void pullold(Node* t)
+//{
+//    if (t == NULL)
+//        return;
+//    push(t);
+//    if (t->left)
+//    {
+//        push(t->left);
+//    }
+//    if (t->right)
+//    {
+//        push(t->right);
+//    }
+//
+//    t->size = 1 + getSize(t->left) + getSize(t->right);
+//    t->timeSum = t->bootTime + getTimeSum(t->left) + getTimeSum(t->right);
+//    // update the data for operation 5
+//    // left and right, fight!
+//    maxtmp = getMaxBT(t->left) > getMaxBT(t->right) ? getMaxBT(t->left) : getMaxBT(t->right);
+//    bool isleft = getMaxBT(t->left) > getMaxBT(t->right) ? true : false;
+//    bool isequal = getMaxBT(t->left) == getMaxBT(t->right) ? true : false;
+//    if (isequal)
+//    {
+//        maxcnttmp = getMaxCnt(t->left) + getMaxCnt(t->right);
+//    }
+//    else
+//    {
+//        if (isleft)
+//        {
+//            maxcnttmp = getMaxCnt(t->left);
+//        }
+//        else
+//        {
+//            maxcnttmp = getMaxCnt(t->right);
+//        }
+//    }
+//
+//    if (isequal)
+//    {
+//        bool issecondequal = getSecondBT(t->left) == getSecondBT(t->right) ? true : false;
+//        if (issecondequal)
+//        {
+//            secondtmp = getSecondBT(t->left);
+//        }
+//        else
+//        {
+//            bool issecondleft = getSecondBT(t->left) > getSecondBT(t->right) ? true : false;
+//            if (issecondleft)
+//            {
+//                secondtmp = getSecondBT(t->left);
+//            }
+//            else
+//            {
+//                secondtmp = getSecondBT(t->right);
+//            }
+//        }
+//    }
+//    else
+//    {
+//
+//        if (isleft)
+//        {
+//            // left is bigger, select right as second
+//            // compare the left's second and the max of right
+//            secondtmp = getSecondBT(t->left) > getMaxBT(t->right) ? getSecondBT(t->left) : getMaxBT(t->right);
+//        }
+//        else
+//        {
+//            // right is bigger, select left as second
+//            //             //secondtmp = getMaxBT(t->left);
+//            // compare the right's second and the max of left
+//            secondtmp = getSecondBT(t->right) > getMaxBT(t->left) ? getSecondBT(t->right) : getMaxBT(t->left);
+//        }
+//    }
+//
+//    // root and bigger, fight!
+//    bool isroot = t->bootTime > maxtmp ? true : false;
+//    isequal = t->bootTime == maxtmp ? true : false;
+//    if (isequal)
+//    {
+//        t->maxBT = t->bootTime;
+//    }
+//    else
+//    {
+//        if (isroot)
+//        {
+//            t->maxBT = t->bootTime;
+//        }
+//        else
+//            t->maxBT = maxtmp;
+//    }
+//
+//    if (isequal)
+//    {
+//        t->maxCnt = maxcnttmp + 1;
+//    }
+//    else
+//    {
+//        if (isroot)
+//        {
+//            t->maxCnt = 1;
+//        }
+//        else
+//        {
+//            t->maxCnt = maxcnttmp;
+//        }
+//
+//    }
+//    if (isequal)
+//    {
+//        t->secondBT = secondtmp;
+//    }
+//    else
+//    {
+//        if (isroot)
+//        {
+//            t->secondBT = maxtmp;
+//        }
+//        else
+//        {
+//            t->secondBT = t->bootTime > secondtmp ? t->bootTime : secondtmp;
+//        }
+//    }
+//}
+
+// ref: discord, three types of situation
+void upgradeCheck(Node* t, long long k)
+{
+    if (t == NULL)
+        return;
+    pushRev(t);
+    pushLazy(t);
+    if (k >= t->maxBT)
+    {
+        return;
+        // does nothing
+    }
+    else if (k >= t->secondBT)
+    {
+        //t->timeSum -= t->maxCnt * (t->maxBT - k);
+        jiry(t, k);
+        pull(t);
+        // whether to go down? no
+    }
+    else if (k < t->secondBT)
+    {
+        //t->lazy = k;
+        //recursiveUpgradeMODI(t);
+        t->bootTime = t->bootTime < k ? t->bootTime : k;
+        upgradeCheck(t->left, k);
+        upgradeCheck(t->right, k);
+        pull(t);
+    }
+    else
+    {
+        if (debugFlag)
+        {
+            printf("wrong");
+        }
+    }
+}
+
 
 Node* merge(Node* a, Node* b)
 {
@@ -102,33 +405,38 @@ Node* merge(Node* a, Node* b)
         return a ? a : b;
     if (a->priority > b->priority)
     {
-        push(a);
+        pushRev(a);
+        pushLazy(a);
         a->right = merge(a->right, b);
         pull(a);
         return a;
     }
     else
     {
-        push(b);
+        pushRev(b);
+        pushLazy(b);
         b->left = merge(a, b->left);
         pull(b);
         return b;
     }
 }
 
-void split(Node* t, int k, Node** a, Node** b)
+void split(Node* t, long long k, Node** a, Node** b)
 {
     if (t == NULL)
     {
         *a = *b = NULL;
         return;
     }
-    push(t);
+    pushRev(t);
+    pushLazy(t);
     // the size of this node's left subtree is bigger than k
     if (getSize(t->left) >= k)
     {
         *b = t;
-        push(*b);
+        // maybe can be comment
+        pushRev(*b);
+        pushLazy(*b);
         // point b to the whole tree, hand over to left subtree
         split(t->left, k, a, &((*b)->left));
         pull(*b);
@@ -136,13 +444,14 @@ void split(Node* t, int k, Node** a, Node** b)
     else
     {
         *a = t;
-        push(*a);
+        pushRev(*a);
+        pushLazy(*a);
         split(t->right, k - getSize(t->left) - 1, &((*a)->right), b);
         pull(*a);
     }
 }
 
-Node* newNode(int bT)
+Node* newNode(long long bT)
 {
     Node* temp = malloc(sizeof(Node));
     temp->priority = rand();
@@ -152,28 +461,20 @@ Node* newNode(int bT)
     temp->rev = 0;
     temp->bootTime = bT;
     temp->timeSum = bT;
+    temp->maxBT = bT;
+    temp->secondBT = 0;
+    temp->maxCnt = 1;
     return temp;
 }
 
-//int kth(Node* t, int k)
-//{
-//    //int lsz = getSize(t->left) + 1;
-//    //if (lsz < k)
-//    //    return kth(t->right, k - lsz);
-//    //else if (lsz == k)
-//    //    return t->serialNum;
-//    //else
-//    //    return kth(t->left, k);
-//}
-
-Node* insertNode(Node* root, int key, int bT)
+Node* insertNode(Node* root, long long key, long long bT)
 {
     Node* lt = 0, * rt = NULL;
     split(root, key, &lt, &rt);
     return merge(merge(lt, newNode(bT)), rt);
 }
 
-Node* addNode(Node* root, int place, int bT)
+Node* addNode(Node* root, long long place, long long bT)
 {
     Node* temp = 0, * temp2 = 0;
     Node* lt = 0, * rt = 0;
@@ -188,7 +489,7 @@ Node* addNode(Node* root, int place, int bT)
 }
 
 
-Node* deleteNode(Node* root, int key)
+Node* deleteNode(Node* root, long long key)
 {
     Node* temp = 0;
     Node* lt = 0, * rt = 0, *lt2, *rt2;
@@ -199,7 +500,7 @@ Node* deleteNode(Node* root, int key)
     return temp;
 }
 
-Node* reverseNode(Node* root, int l, int r)
+Node* reverseNode(Node* root, long long l, long long r)
 {
     Node* lt = 0, * rt = 0, * lt2, * rt2;
     split(root, l - 1, &lt, &rt);
@@ -208,136 +509,134 @@ Node* reverseNode(Node* root, int l, int r)
     return merge(merge(lt, lt2), rt2);
 }
 
-Node* swapNode(Node* root, int l, int r, int x, int y)
+long long tmpv;
+Node* swapNode(Node* root, long long l, long long r, long long x, long long y)
 {
+    if (l > x)
+    {
+        // swap the numbers
+        tmpv = l;
+        l = x;
+        x = tmpv;
+        tmpv = r;
+        r = y;
+        y = tmpv;
+    }
     Node* lt = 0, * rt = 0, * lt2, * rt2, * lt3, *rt3, *lt4, *rt4;
     split(root, l - 1, &lt, &rt);
     split(rt, r - l + 1, &lt2, &rt2);
-
     split(rt2, x - r - 1, &lt3, &rt3);
     split(rt3, y - x + 1, &lt4, &rt4);
     return merge(merge(merge(merge(lt, lt4), lt3), lt2), rt4);
 }
 
-Node* upgradeNode(Node* root, int l, int r, int k)
+Node* upgradeNode(Node* root, long long l, long long r, long long k)
 {
-    //Node* lt, * rt, * lt2, * rt2;
-    //split(root, l - 1, &lt, &rt);
-    //split(rt, r - l + 1, &lt2, &rt2);
-
-    //inorderupgrade(lt2, k);
-
-    //return merge(lt, merge(lt2, rt2));
+    Node* lt, * rt, * lt2, * rt2;
+    split(root, l - 1, &lt, &rt);
+    split(rt, r - l + 1, &lt2, &rt2);
+    upgradeCheck(lt2, k);
+    return merge(lt, merge(lt2, rt2));
 }
 
-Node* query(Node* root, int l, int r, long long* q)
+
+Node* query(Node* root, long long l, long long r, long long* q)
 {
     Node *lt, *rt, *lt2, *rt2;
     Node* nuroot;
     split(root, l - 1, &lt, &rt);
-    //printTreap(rt, true);
     split(rt, r - l + 1, &lt2, &rt2);
     *q = lt2->timeSum;
     nuroot = merge(lt, merge(lt2, rt2));
     return nuroot;
 }
 
-int inorderupgrade(Node* root, int target)
-{
-    // Base Cases: root is null or key is present at root
-    if (root == NULL)
-        return 0;
-    inorderupgrade(root->left, target);
-    inorderupgrade(root->right, target);
-}
 
 
 int main() {
     srand(7777777);
-    int sr;
+    long long sr;
     FILE* ptr = 0;
     if (fileFlag)
     {
-        ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW4\\HW4\\hw4_testdata\\P4\\4.in", "r");
-        sr = fscanf(ptr, "%d %d", &numMachineN, &numRecordQ);
+        ptr = fopen("D:\\Senior_Spring\\DSA\\NTUCSIE-2022-DSA-Assignments\\HW4\\HW4\\hw4_testdata\\P4\\7.in", "r");
+        sr = fscanf(ptr, "%lld %lld", &numMachineN, &numRecordQ);
     }
     else
-        sr = scanf("%d %d", &numMachineN, &numRecordQ);
+        sr = scanf("%lld %lld", &numMachineN, &numRecordQ);
 
 
     server = NULL;
-    int bT = 0;
-    for (int i = 1; i < numMachineN + 1; i++)
+    long long bT = 0;
+    for (long long i = 1; i < numMachineN + 1; i++)
     {
         if (fileFlag)
-            sr = fscanf(ptr, "%d", &bT);
+            sr = fscanf(ptr, "%lld", &bT);
         else
-            sr = scanf("%d", &bT);
+            sr = scanf("%lld", &bT);
         server = insertNode(server, i, bT);
     }
     // high priority at top
     if (debugFlag)
+    {
+        printf("Initial server: \n");
         printTreap(server, true);
+    }
 
 
-    int type;
-    int p, k, l, r, x, y;
+    long long type;
+    long long p, k, l, r, x, y;
     long long q;
-    for (int i = 1; i < (numRecordQ + 1); i++)
+    for (long long i = 1; i < (numRecordQ + 1); i++)
     {
         if (fileFlag)
-            sr = fscanf(ptr, "%d", &type);
+            sr = fscanf(ptr, "%lld", &type);
         else
-            sr = scanf("%d", &type);
+            sr = scanf("%lld", &type);
 
-        switch (type)
+         switch (type)
         {
         case 1:
             if (fileFlag)
-                sr = fscanf(ptr, "%d %d", &p, &k);
+                sr = fscanf(ptr, "%lld %lld", &p, &k);
             else
-                sr = scanf("%d %d", &p, &k);
+                sr = scanf("%lld %lld", &p, &k);
             // insert between p, p+1, with bT = k
             server = addNode(server, p, k);
-            if (debugFlag)
-                printTreap(server, true);
             break;
         case 2:
             if (fileFlag)
-                sr = fscanf(ptr, "%d", &p);
+                sr = fscanf(ptr, "%lld", &p);
             else
-                sr = scanf("%d", &p);
-            // p: is the serial number
+                sr = scanf("%lld", &p);
             server = deleteNode(server, p);
-            if (debugFlag)
-                printTreap(server, true);
             break;
         case 3:
             if (fileFlag)
-                sr = fscanf(ptr, "%d %d", &l, &r);
+                sr = fscanf(ptr, "%lld %lld", &l, &r);
             else
-                sr = scanf("%d %d", &l, &r);
+                sr = scanf("%lld %lld", &l, &r);
             server = reverseNode(server, l, r);
             break;
         case 4:
             if (fileFlag)
-                sr = fscanf(ptr, "%d %d %d %d", &l, &r, &x, &y);
+                sr = fscanf(ptr, "%lld %lld %lld %lld", &l, &r, &x, &y);
             else
-                sr = scanf("%d %d %d %d", &l, &r, &x, &y);
+                sr = scanf("%lld %lld %lld %lld", &l, &r, &x, &y);
             server = swapNode(server, l, r, x, y);
             break;
         case 5:
             if (fileFlag)
-                sr = fscanf(ptr, "%d %d %d", &l, &r, &k);
+                sr = fscanf(ptr, "%lld %lld %lld", &l, &r, &k);
             else
-                sr = scanf("%d %d %d", &l, &r, &k);
-            //server = upgradeNode(server, l, r, k);
+                sr = scanf("%lld %lld %lld", &l, &r, &k);
+            server = upgradeNode(server, l, r, k);
             break;
         case 6:
             if (fileFlag)
-                sr = fscanf(ptr, "%d %d", &l, &r);
+                sr = fscanf(ptr, "%lld %lld", &l, &r);
             else
-                sr = scanf("%d %d", &l, &r);
+                sr = scanf("%lld %lld", &l, &r);
             server = query(server, l, r, &q);
             printf("%lld\n", q);
             break;
@@ -345,7 +644,11 @@ int main() {
             break;
         }
         if (debugFlag)
+        {
+            printf("op: %lld, type: %lld \n", i, type);
             printTreap(server, true);
+            printf("--++--++--\n");
+        }
     }
     if (fileFlag)
     {
